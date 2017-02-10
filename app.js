c973e1e2bbd06e764185d62e0135f0aa072e270f
Pmookerjee <pmookerjee@gmail.com>
2017-02-09 20:19:07 -0800

    Added initial files and scaffolding for app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+var PikePlace = {
+
+  this.min: 17,
+  this.max: 88,
+  this.avg: 5.2,
+  this.getRandom: function(min = minxCustPerHr){
+    return Math.random() * ((max - min) + min)},
+  this.getCookiesPerHr: function() {
+
+  },
+  this.storeCookiesPerHr: function(){
+
+  },
+  this.renderData: function() {
+    // render your object data to the DOM as an unordered list of data points!!
+    // 1. Get an element by Id from your html
+    // 2. Create a UL element
+    // 3. Create and populate an LI element for each of the data points that you want to render
+    // 4. Append the LI to the UL
+    // 5. Append the UL to the element that you stored in step 1
+  }
+};
