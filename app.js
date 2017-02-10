487d894769b23c5273374acbd5951d715aa7c892
Pmookerjee <pmookerjee@gmail.com>
2017-02-09 22:06:49 -0800

    Ermahgerd... it works!

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var PikePlace = {
   min: 17,
   max: 88,
   avg: 5.2,
-  cookiesPerHr:0,
+  cookiesPerHr:[],
   getRandom: function(min, max){
     min = this.min;
     max = this.max;
@@ -10,22 +10,31 @@ var PikePlace = {
   },
   getCookiesPerHr: function(num) {
     var cookies = num * this.avg;
-    this.cookiesPerHr = cookies.toFixed(0);
+    this.cookiesPerHr.push(cookies.toFixed(0));
      console.log('cookies per hour is ' + this.cookiesPerHr);
   },
-  storeCookiesPerHr: function(){
-
-  },
-  renderData: function() {
-    // render your object data to the DOM as an unordered list of data points!!
-    // 1. Get an element by Id from your html
+  renderData: function(hour, index) {
+    var header = document.getElementById('location');
+    var newUl = document.createElement('ul');
+    var newLi = document.createElement('li');
+    var content = hour + ': ' + this.cookiesPerHr[index] + ' cookies';
+    var newLiText = document.createTextNode(content);
+    newLi.appendChild(newLiText);
+    newUl.appendChild(newLi);
+    header.appendChild(newUl);
     // 2. Create a UL element
     // 3. Create and populate an LI element for each of the data points that you want to render
     // 4. Append the LI to the UL
     // 5. Append the UL to the element that you stored in step 1
   }
 };
-var randomNum = PikePlace.getRandom();
-console.log(randomNum);
-PikePlace.getCookiesPerHr(randomNum);
+
+for(var i = 0; i < 8; i++){
+  var hour = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
+  var randomNum = PikePlace.getRandom();
+  console.log(randomNum);
+  PikePlace.getCookiesPerHr(randomNum);
+  PikePlace.renderData(hour[i], i);
+}
+
 //var el = getElementById('')
