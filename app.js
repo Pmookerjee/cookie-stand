a79e2cd23453fd3a8fb90a2c357613df3acbe8ad
Pmookerjee <pmookerjee@gmail.com>
2017-02-11 15:06:11 -0800

    removed for loop and simplifying in interest of time since it wasnt working

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+
+
 var PikePlace = {
   min: 17,
   max: 88,
@@ -15,7 +18,7 @@ var PikePlace = {
     console.log('cookies per hour is ' + this.cookiesPerHr);
   },
   renderData: function(hour, index) {
-    var header = document.getElementById('location');
+    var header = document.getElementById('PikePlace');
     var newUl = document.createElement('ul');
     var newLi = document.createElement('li');
     var content = hour + ': ' + this.cookiesPerHr[index] + ' cookies';
@@ -27,7 +30,7 @@ var PikePlace = {
     //If we have just computed our last hour, render the total number of cookies
     if(index === 7) {
       var total = 'Total: ' + this.totalCookies + ' cookies';
-      var header = document.getElementById('location');
+      var header = document.getElementById('PikePlace');
       var newUl = document.createElement('ul');
       var newLi = document.createElement('li');
       var newLiText = document.createTextNode(total);
@@ -42,14 +45,61 @@ var PikePlace = {
   }
 };
 
-//Put an array in here and another loop for the location names....
-for(var i = 0; i < 8; i++){
-  var hour = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
-  var randomNum = PikePlace.getRandom();
-  console.log(randomNum);
-  PikePlace.getCookiesPerHr(randomNum);
-  PikePlace.addToTotal(i);
-  PikePlace.renderData(hour[i], i);
-}
+// var SeaTac = {
+//   min: 6,
+//   max: 24,
+//   avg: 1.2,
+//   cookiesPerHr:[],
+//   totalCookies:0,
+//   getRandom: function(min, max){
+//     min = this.min;
+//     max = this.max;
+//     return Math.round(Math.random() * (max-min) + min);
+//   },
+//   getCookiesPerHr: function(num) {
+//     var cookies = num * this.avg;
+//     this.cookiesPerHr.push(cookies.toFixed(0));
+//   },
+//   renderData: function(hour, index){
+//     var header = document.getElementById('SeaTac');
+//     var newUl = document.createElement('ul');
+//     var newLi = document.createElement('li');
+//     var content = hour + ': ' + this.cookiesPerHr[index] + ' cookies';
+//     var newLiText = document.createTextNode(content);
+//     newLi.appendChild(newLiText);
+//     newUl.appendChild(newLi);
+//     header.appendChild(newUl);
+//
+//     //If we have just computed our last hour, render the total number of cookies
+//     if(index === 7) {
+//       var total = 'Total: ' + this.totalCookies + ' cookies';
+//       var header = document.getElementById('SeaTac');
+//       var newUl = document.createElement('ul');
+//       var newLi = document.createElement('li');
+//       var newLiText = document.createTextNode(total);
+//       newLi.appendChild(newLiText);
+//       newLi.appendChild(newLiText);
+//       newUl.appendChild(newLi);
+//       header.appendChild(newUl);
+//     }
+//   },
+//   addToTotal: function(index){
+//     this.totalCookies += +this.cookiesPerHr[index];
+//   }
+// };
 
-//var el = getElementById('')
+//Put an array in here and another loop for the location names....
+//also, add beginning and endvalue variables
+var locationName = ['PikePlace', 'SeaTac', 'SouthCenter', 'BellevueSquare', 'Alki'];
+
+for(var j = 0; j<locationName[j].length; j++){
+  for(var i = 0; i < 8; i++){
+    var hour = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
+    console.log('location is ' + locationName[j]);
+    var location2 = locationName[j];
+    var randomNum = location2.getRandom();
+    location2.getCookiesPerHr(randomNum);
+    location2.addToTotal(i);
+    location2.renderData(hour[i], i);
+  }
+}
