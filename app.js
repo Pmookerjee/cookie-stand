1e2eab5c0208cfa9235de49f7a7b644b57de3e78
Pmookerjee <pmookerjee@gmail.com>
2017-02-12 21:43:19 -0800

    All the location data is rendering now

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
+var headers = ['10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm']
+var
 
+//Initialize the Location objects...
+var PikePlace = new Location('Pike Place', 17, 88, 5.2);
+var SeaTac = new Location('SeaTac Airport', 6, 24, 1.2);
+var Southcenter = new Location('Southcenter', 11, 38, 1.9);
+var BellevueSquare = new Location('Bellevue Square', 20, 48, 3.3);
+var Alki = new Location('Alki', 3, 24, 2.6);
 
+//Store the objects in an array
+var store = [PikePlace, SeaTac, Southcenter, BellevueSquare, Alki];
 
-function Location(name, min, max, avg, storeId) {
+//Draw the table header once
+renderTableHeader(headers);
+
+for(var j=0; j<store.length; j++){
+  for (var i=0; i<headers.length; i++){
+    var randomNum = store[j].getRandom();
+    store[j].getCookiesPerHr(randomNum);
+    store[j].addToTotal(i);
+  }
+  store[j].renderTableBody();
+}
+
+function Location(name, min, max, avg) {
   this.name = name;
   this.min = min;
   this.max = max;
   this.avg = avg;
-  this.storeId = storeId;
   this.cookiesPerHr = [];
   this.totalCookies = 0;
 
@@ -16,17 +37,8 @@ function Location(name, min, max, avg, storeId) {
   this.getCookiesPerHr = function(num) {
     var cookies = num * this.avg;
     this.cookiesPerHr.push(cookies.toFixed(0));
-    console.log('cookies per hour is ' + this.cookiesPerHr);
   };
-  this.renderTableHeader = function(headerNames){
-    var header = document.getElementById('table_header');
-      for (var i=0; i<headerNames.length; i++){
-        var newTh = document.createElement('th');
-        var headerName = document.createTextNode(headerNames[i]);
-        newTh.appendChild(headerName);
-        header.appendChild(newTh);
-      }
-  };
+
   this.renderTableBody = function() {
     //check if location name has already been displayed in table row
       var tbody = document.getElementById('table_body');
@@ -36,6 +48,7 @@ function Location(name, min, max, avg, storeId) {
       newTd.appendChild(content);
       newTr.appendChild(newTd);
       tbody.appendChild(newTr);
+      console.log('cookiesPerHr is ' + this.cookiesPerHr.length)
       for(var i=0; i<this.cookiesPerHr.length; i++){
         var newTd = document.createElement('td');
         var cookieData = document.createTextNode(this.cookiesPerHr[i]);
@@ -43,25 +56,6 @@ function Location(name, min, max, avg, storeId) {
         newTr.appendChild(newTd);
         tbody.appendChild(newTr);
       }
-
-
-
-    // newLi.appendChild(newLiText);
-    // newUl.appendChild(newLi);
-    // header.appendChild(newUl);
-
-    //If we have just computed our last hour, render the total number of cookies
-    // if(index ===lastHour) {
-    //   var total = 'Total: ' + this.totalCookies + ' cookies';
-    //   var header = document.getElementById(this.storeId);
-    //   var newUl = document.createElement('ul');
-    //   var newLi = document.createElement('li');
-    //   var newLiText = document.createTextNode(total);
-    //   newLi.appendChild(newLiText);
-    //   newLi.appendChild(newLiText);
-    //   newUl.appendChild(newLi);
-    //   header.appendChild(newUl);
-    // }
   };
   this.renderTableFooter = function(){};
   this.addToTotal = function(index){
@@ -69,30 +63,12 @@ function Location(name, min, max, avg, storeId) {
   };
 }
 
-//Put an array in here and another loop for the location names....
-//also, add beginning and endvalue variables
-//var locationName = ['PikePlace', 'SeaTac', 'SouthCenter', 'BellevueSquare', 'Alki'];
-
-var headers = ['      ', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm']
-var firstHour = 0
-var lastHour = 7;
-var PikePlace = new Location('Pike Place', 17, 88, 5.2, 'PikePlace');
-var store = [PikePlace];
-
-
-for (var i=0; i<8; i++){
-  var randomNum = PikePlace.getRandom();
-  PikePlace.getCookiesPerHr(randomNum);
-  PikePlace.addToTotal(i);
-}
-PikePlace.renderTableHeader(headers);
-PikePlace.renderTableBody();
-
-// for (var j=0; j<store.length; j++){
-//   for(var i = firstHour; i <= lastHour; i++){
-//     var randomNum = store[j].getRandom();
-//     store[j].getCookiesPerHr(randomNum);
-//     store[j].addToTotal(i);
-//     store[j].renderData(hour[i], i);
-//   }
-// }
+function renderTableHeader(headerNames){
+  var header = document.getElementById('table_header');
+    for (var i=0; i<headerNames.length; i++){
+      var newTh = document.createElement('th');
+      var headerName = document.createTextNode(headerNames[i]);
+      newTh.appendChild(headerName);
+      header.appendChild(newTh);
+    }
+};
