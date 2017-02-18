a94474d77ffa0433f5063ef8ef7f0e4bd0a1aa55
Pmookerjee <pmookerjee@gmail.com>
2017-02-18 12:10:42 -0800

    Added scaffolding for nav bar

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var headers = ['      ','10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm']
 //Totals holds all the cookiesPerHr arrays for every store
 var totals = [];
+//Store holds all the Location objects
 var store = [];
 
 //Initialize the Location objects...
@@ -9,34 +10,11 @@ var SeaTac = new Location('SeaTac Airport', 6, 24, 1.2);
 var Southcenter = new Location('Southcenter', 11, 38, 1.9);
 var BellevueSquare = new Location('Bellevue Square', 20, 48, 3.3);
 var Alki = new Location('Alki', 3, 24, 2.6);
+
+//index for totals[], for event form submit
 var row = 0;
 
-
-
-//Run all the things...
-(function runTheApp() {
-
-  //First draw the table header once
-  renderTableHeader(headers);
-
-  //Iterate though all the stores, get the total cookies per hour
-  for(var j=0; j<store.length; j++){
-    for (var i=0; i<headers.length-1; i++){
-      var randomNum = store[j].getRandom();
-      store[j].getCookiesPerHr(randomNum);
-      store[j].addToTotal(i);
-    }
-    //Store the totals for all stores in a 2d array
-    totals[j] = store[j].cookiesPerHr;
-    //Draw the table
-    store[j].renderTableRow();
-  }
-  row = j-1;
-  renderTableFooter(totals);
-})();
-
 form.addEventListener('submit', handleFormSubmit);
-
 
 function Location(name, min, max, avg) {
   this.name = name;
@@ -46,39 +24,39 @@ function Location(name, min, max, avg) {
   this.cookiesPerHr = [];
   this.totalCookies = 0;
   store.push(this);
+}
 
-  this.getRandom = function(){
-    var beans = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-    return beans;
-  };
+Location.prototype.getRandom = function(){
+  var beans = Math.floor(Math.random() * (this.max - this.min)) + this.min;
+  return beans;
+};
 
-  this.getCookiesPerHr = function(num) {
-    var cookies = num * this.avg;
-    this.cookiesPerHr.push(cookies.toFixed(0));
-  };
+Location.prototype.getCookiesPerHr = function(num) {
+  var cookies = num * this.avg;
+  this.cookiesPerHr.push(cookies.toFixed(0));
+};
 
-  this.renderTableRow = function() {
-    //check if location name has already been displayed in table row
-    var tbody = document.getElementById('table_body');
-    var newTr = document.createElement('tr');
+Location.prototype.renderTableRow = function() {
+  //check if location name has already been displayed in table row
+  var tbody = document.getElementById('table_body');
+  var newTr = document.createElement('tr');
+  var newTd = document.createElement('td');
+  var content = document.createTextNode(this.name);
+  newTd.appendChild(content);
+  newTr.appendChild(newTd);
+  tbody.appendChild(newTr);
+  for(var i=0; i<this.cookiesPerHr.length; i++){
     var newTd = document.createElement('td');
-    var content = document.createTextNode(this.name);
-    newTd.appendChild(content);
+    var cookieData = document.createTextNode(this.cookiesPerHr[i]);
+    newTd.appendChild(cookieData);
     newTr.appendChild(newTd);
     tbody.appendChild(newTr);
-    for(var i=0; i<this.cookiesPerHr.length; i++){
-      var newTd = document.createElement('td');
-      var cookieData = document.createTextNode(this.cookiesPerHr[i]);
-      newTd.appendChild(cookieData);
-      newTr.appendChild(newTd);
-      tbody.appendChild(newTr);
-    }
-  };
+  }
+};
 
-  this.addToTotal = function(index){
-    this.totalCookies += +this.cookiesPerHr[index];
-  };
-}
+Location.prototype.addToTotal = function(index){
+  this.totalCookies += +this.cookiesPerHr[index];
+};
 
 function renderTableHeader(headerNames){
   var header = document.getElementById('table_header');
@@ -90,12 +68,17 @@ function renderTableHeader(headerNames){
   }
 };
 
+function deleteTableFooter(){
+  var removeTr = document.getElementById('totals_row');
+  var containerTr = removeTr.parentNode;
+  containerTr.removeChild(removeTr);
+}
+
 function renderTableFooter(t) {
-  if(document.getElementById('totals_row')) {
-    var removeTr = document.getElementById('totals_row');
-    var containerTr = removeTr.parentNode;
-    containerTr.removeChild(removeTr);
-  }
+  //Check if Totals row exists: if so remove the row...
+  if(document.getElementById('totals_row')) { deleteTableFooter(); }
+
+  //Draw first only the Row header called 'Totals'
   var tbody = document.getElementById('table_footer');
   var newTr = document.createElement('tr');
   newTr.setAttribute('id', 'totals_row')
@@ -113,6 +96,7 @@ function renderTableFooter(t) {
       total += +t[j][i];
     }
 
+    //Render the content of the current total
     var newTd = document.createElement('td');
     newTd.setAttribute('id', 'totals');
     var cookieData = document.createTextNode(total);
@@ -134,15 +118,17 @@ function handleFormSubmit(event) {
 
   var newLocation = new Location(location, min, max, avg);
 
+  //Do the calculations on the new user inputed data
   for (var i=0; i<headers.length-1; i++){
     randomNum = newLocation.getRandom();
     newLocation.getCookiesPerHr(randomNum);
     newLocation.addToTotal(i);
   }
+  //Add new row data to the totals[]
   totals[row] = newLocation.cookiesPerHr;
+  //Draw the new row
   newLocation.renderTableRow();
-
-  //Draw the totals per hour
+  //Draw the new totals per time of day
   renderTableFooter(totals);
 
   event.target.location.value = null;
@@ -150,3 +136,28 @@ function handleFormSubmit(event) {
   event.target.max.value = null;
   event.target.avg.value = null;
 }
+
+//Run all the things...
+(function runTheApp() {
+
+  //First draw the table header once
+  renderTableHeader(headers);
+
+  //Iterate though all the stores, get the total cookies per hour
+  for(var j=0; j<store.length; j++){
+    for (var i=0; i<headers.length-1; i++){
+      var randomNum = store[j].getRandom();
+      store[j].getCookiesPerHr(randomNum);
+      store[j].addToTotal(i);
+    }
+    //Store the totals for all stores in a 2d array
+    totals[j] = store[j].cookiesPerHr;
+    //Draw the table
+    store[j].renderTableRow();
+  }
+  //Set row to be the last row added to totals[]
+  row = j-1;
+
+  //Draw the totals for each column
+  renderTableFooter(totals);
+})();
